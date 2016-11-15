dcde53d97026fba94baa2d1840aea8cbaa79a1f0
Rizadh Nizam <rizadh@icloud.com>
2016-11-14 20:04:09 -0500

    Improve prompt message, indicating defaults

--- particle.js
+++ particle.js
@@ -1,16 +1,17 @@
 // Options
-const maxBalls = parseInt(prompt("How many balls can ya handle?")) || 10;
-const ballSize = parseInt(prompt("How big should they be? (px)")) || 5;
-const maxSpeed = parseFloat(prompt("How fast do you want 'em? (px/s)")) || 100;
+const maxBalls = parseInt(prompt("How many balls can ya handle?\n\nDefault: 100")) || 100;
+const ballSize = parseInt(prompt("How big should they be? (px)\n\nDefault: 10")) || 10;
+const maxSpeed = parseFloat(prompt("How fast do you want 'em? (px/s)\n\nDefault: 100")) || 100;
 const maxAccel = parseFloat(prompt(
     "How much energy do you want to give 'em? (px/s\u00B2)\n\n" +
-    `I recommend ${maxSpeed / 2}`
-)) || maxSpeed / 2;
+    "Default: " + parseInt(maxSpeed / 4)
+)) || maxSpeed / 4;
 const areaRestrictFactor = Math.min(Math.max(parseFloat(prompt(
     "How much of the screen should they cover?\n\n" +
     "0 = only right in the middle (boring)\n" +
     "0.5 = half the screen (interesting)\n" +
-    "1 = the entire screen (fun)"
+    "1 = the entire screen (fun)\n\n" +
+    "Default: 1"
 )), 0), 1) || 1;
 
 const gravity = !confirm("Disable gravity?");
